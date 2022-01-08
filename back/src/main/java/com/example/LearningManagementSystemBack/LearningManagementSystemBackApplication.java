b7c839ec20586ec585b919ce918fa4eef68740d3
cantuc40 <cantuc40@gator.uhd.edu>
2022-01-07 19:59:22 -0600

    backend initialized

---
+++ back/src/main/java/com/example/LearningManagementSystemBack/LearningManagementSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningManagementSystemBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManagementSystemBackApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/LearningManagementSystemBack/LearningManagementSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningManagementSystemBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
